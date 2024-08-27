dbd4cedaf8b842373f51ebe48098165cb1588ad6
Your Name <you@example.com>
2024-08-27 22:01:02 +0800

    refresh ultrasonic

---
+++ robot/Arduino_ultrasonic/arduino_ultrasonic/commands.h
@@ -0,0 +1,29 @@
+/* Define single-letter commands that will be sent by the PC over the
+   serial link.
+*/
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#define ANALOG_READ    'a'
+#define GET_BAUDRATE   'b'
+#define PIN_MODE       'c'
+#define DIGITAL_READ   'd'
+#define MOTOR_RAW_PWM  'o'
+#define SERVO_WRITE    's'
+#define SERVO_READ     't'
+#define ULTRA_SONIC_VALUE 'u'
+#define DIGITAL_WRITE  'w'
+#define ANALOG_WRITE   'x'
+
+#define L1             1
+#define L2             2
+#define L3             3
+#define R1             4
+#define R2             5
+#define R3             6
+
+
+#endif
+
+
