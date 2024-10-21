bd2912aeedc516ac48eddab5a49fb9fb839c2e73
name <example@gmail.com>
2024-10-21 23:02:03 +0800

    TEST1

--- main/jetson_python/test2.py
+++ main/jetson_python/test2.py
@@ -6,5 +6,8 @@ while True:
     color = goose_weight.color_block_detect(frame)
     print(f"Color: {color}")
 
+    if color == goose_weight.COLOR_BLUE:
+        pass
+
     if cv2.waitKey(33) & 0xFF == ord('q'):
         break
